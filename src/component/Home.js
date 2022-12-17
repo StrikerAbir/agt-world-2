7385ea4e3374df19cfb97a8e88a42328dceb1325
Abir Hasan <cse.170201063@gmail.com>
2022-12-17 07:06:11 +0600

    posting done

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthProvider';
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
+    
+    const handleOnsubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const post = form.post.value;
+        const data={name:user.displayName, email: user.email, post, like:0}
+        console.log(data);
+        fetch("http://localhost:1000/posts", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data));
+    }
     return (
-        <div>
-            aslfs
-        </div>
+      <div className='mt-10'>
+        <form onSubmit={handleOnsubmit}>
+          <div className="flex justify-center items-center">
+            <textarea
+              className="textarea textarea-bordered w-96 h-24"
+                        placeholder="post"
+                        name='post'
+            ></textarea>
+            <input type="submit" value="Post" className="btn btn-sm ml-5" />
+          </div>
+        </form>
+      </div>
     );
 };
 
